b31c81b6d4f9b95a318d0813730cf9c8e66f3820
Jacques Clément <jacquesclement@icloud.com>
2019-06-19 14:00:50 +0200

    URLs + Views for User API

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls import include
+
 """app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('user.urls')),
 ]
